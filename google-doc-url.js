6dcb60bb3bf5daf8a9864c4bf4b4c15cbca4276e
Nolan Cassidy <nolancassidy1@gmail.com>
2019-05-29 13:25:43 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1q4BuUA63Eg3Z612C0Uu7ek99Mg2DqnaI4N6F6lyUEKs/edit#gid=0';
