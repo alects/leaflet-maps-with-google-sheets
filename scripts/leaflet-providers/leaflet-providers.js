6e75e321dbf20537376d22e3244793ed5692a2aa
Alex Petzold <46435229+alects@users.noreply.github.com>
2019-05-29 17:08:57 -0700

    Update leaflet-providers.js

--- scripts/leaflet-providers/leaflet-providers.js
+++ scripts/leaflet-providers/leaflet-providers.js
@@ -102,7 +102,8 @@
 		UOregon: {
 			url: 'https://tiles.uoregon.edu/arcgis/colorwithanno/{z}/{x}/{y}.png',
 			options: {
-				maxZoom: 23,
+				maxNativeZoom: 25,
+				maxZoom: 25,
 				attribution:
 					"<a href='http://infographics.uoregon.edu/services/campus-mapping/' target='_blank'> UOCGIS </a> | <a href='http://leafletjs.com/' target='_blank'>Leaflet</a>"
 			}
