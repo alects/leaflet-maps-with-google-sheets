781d1d2015a39f6858815e9c6f54a38d9edbc383
Brendon-K <brendon.kieser@gmail.com>
2019-05-29 17:37:02 -0700

    Attempting to add uoregon map

--- scripts/leaflet-providers/leaflet-providers.js
+++ scripts/leaflet-providers/leaflet-providers.js
@@ -104,7 +104,7 @@
 			options: {
 				maxNativeZoom: 23,
 				maxZoom: 28,
-				zoomOffset: 5,
+				zoomOffset: -5,
 				attribution:
 					"<a href='http://infographics.uoregon.edu/services/campus-mapping/' target='_blank'> UOCGIS </a> | <a href='http://leafletjs.com/' target='_blank'>Leaflet</a>"
 			}
